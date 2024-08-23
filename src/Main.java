68bf1b0507449760dce99333721ca13b6f5a6063
Igor <118084755+MaxParadiseKing@users.noreply.github.com>
2024-08-23 21:31:08 +0300

    First04

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("My Aqa Project");
+        System.out.printf("My Aqa Project 001");
     }
 }
