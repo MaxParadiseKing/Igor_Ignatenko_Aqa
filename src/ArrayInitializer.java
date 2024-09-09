fe285e5f6832c23c4823f6aa36a9ed7f4ceb8988
Igor <118084755+MaxParadiseKing@users.noreply.github.com>
2024-09-09 19:13:21 +0300

    LissonAQA_dz_04

--- src/ArrayInitializer.java
+++
@@ -1,9 +0,0 @@
-public class ArrayInitializer {
-    public static int[] initializeArray(int len, int initialValue) {
-        int[] arr = new int[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = initialValue;
-        }
-        return arr;
-    }
-}
